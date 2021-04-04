be6976659680101394c73c1d8c0f24206d96085a
Svetlana Rokashevich <sv_ch@live.com>
2021-04-04 16:33:09 +0300

    6

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+const App = () => {
+  return <div>
+    <div>
+      1div
+    </div>
+    <div className="App">
+      <ul>
+        <li>gf</li>
+        <li>gf</li>
+        <li>gf</li>
+        <li>gf</li>
+        <li>gf</li>
+      </ul>
+    </div>
+  </div>
+
+
+}
+
+export default App;
