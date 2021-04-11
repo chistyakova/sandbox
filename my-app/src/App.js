a8cf786f0b4a91fb8c50a5d8b954aee2dc837ee7
Svetlana Rokashevich <sv_ch@live.com>
2021-04-11 21:49:25 +0300

    14

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
+//import React from 'react'
 import './App.css';
-import Header from './Header'
-import Technologies from './Technologies'
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
-  return <div>
-     <Header />
-     <Technologies />
+  return <div className='app-wrapper'>
+    <Header />
+    <Navbar />
+    <Profile />
   </div>
 
 
