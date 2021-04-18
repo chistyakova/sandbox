ba3183e43f40d887fc2eb8ae1f96bee94b9ff584
Svetlana Rokashevich <sv_ch@live.com>
2021-04-18 22:00:29 +0300

    аватарки

--- my-app/src/components/Dialogs/DialogItem/DialogItem.jsx
+++ my-app/src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -1,11 +1,16 @@
 import { NavLink } from 'react-router-dom'
-import style from './../Dialogs.module.css'
+import style from './DialogItem.module.css'
 
 const DialogItem = (props) => {
     let path = '/dialogs/' + props.id;
     return (
-        <div className={style.dialog + ' ' + style.active}>
-            <NavLink to={path}>{props.name}</NavLink>
+        <div className={style.dialogsItem}>
+            <div>
+                <img src={props.avatar} />
+            </div>
+            <div className={style.dialogName}>
+                <NavLink to={path}>{props.name}</NavLink>
+            </div>
         </div>
     )
 }
--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -5,7 +5,7 @@ import Message from './Message/Message'
 const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogs
-        .map(d => <DialogItem name={d.name} id={d.id} />);
+        .map(d => <DialogItem name={d.name} id={d.id} avatar={d.avatar}/>);
 
     debugger;
 
