706c9e6589c3d63e271726635fc54fa63a6e8d0e
Svetlana Rokashevich <sv_ch@live.com>
2021-05-03 22:41:50 +0300

    52

--- my-app/src/components/users/Users.jsx
+++ my-app/src/components/users/Users.jsx
@@ -3,13 +3,16 @@ import style from './Users.module.css'
 import userPhoto from '../../assets/images/user.jpg'
 
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
-        });
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
 
     return <div>
+        <button onClick={getUsers}>Get users</button>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
