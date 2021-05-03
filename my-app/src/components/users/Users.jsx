ea79df7fb1d2d7d65d41e0f1bc2162de46969788
Svetlana Rokashevich <sv_ch@live.com>
2021-05-03 20:08:34 +0300

    51

--- my-app/src/components/users/Users.jsx
+++ my-app/src/components/users/Users.jsx
@@ -1,27 +1,35 @@
+import * as axios from 'axios'
 import style from './Users.module.css'
+import userPhoto from '../../assets/images/user.jpg'
 
 const Users = (props) => {
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        });
+    }
+
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={style.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={style.userPhoto} />
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={ () => { props.unfollow(u.id) } } >unfollow</button>
-                            : <button onClick={ () => { props.follow(u.id) } }>follow</button>}
+                            ? <button onClick={() => { props.unfollow(u.id) }} >unfollow</button>
+                            : <button onClick={() => { props.follow(u.id) }}>follow</button>}
                     </div>
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
             </div>)
