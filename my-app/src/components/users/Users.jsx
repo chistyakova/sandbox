1c229a3b167733a86520a03ebd9c6e3cb5bbc4c9
Svetlana Rokashevich <sv_ch@live.com>
2021-05-04 09:48:29 +0300

    54

--- my-app/src/components/users/Users.jsx
+++ my-app/src/components/users/Users.jsx
@@ -4,8 +4,8 @@ import userPhoto from '../../assets/images/user.jpg'
 import React from 'react'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+    
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items);
         });
