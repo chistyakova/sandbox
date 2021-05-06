fb9628fe0f15e0a969384152bd91bf5dd174fee1
Svetlana Rokashevich <sv_ch@live.com>
2021-05-06 13:36:47 +0300

    60

--- my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ my-app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -10,9 +10,6 @@ const ProfileInfo = (props) => {
         <div>
             <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
         </div>
-        <div>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDkvFCLSMbUU6Bqb1m-0y3LPAQ7_Gcs-PNZw&usqp=CAU' />
-        </div>
         <div className={style.descriptionBlock}>
             <img src={props.profile.photos.large} />
             ava+descr9999
